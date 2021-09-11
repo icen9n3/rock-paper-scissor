2716a95e57c608a802a4db2d3e4a78a2a46bb777
Zak <zak.solowsky@gmail.com>
2021-09-11 01:53:51 -0400

    rps-js.js major rework - of playRound() added working scoring system that outputs to html removed playMatch() - wasn't necessary could not  get a working loop because the players input is what should drive the loop (way out of my scope for this project) moved #score and #round .textContent to game() & playRound()
    need
    formatting improvement (its bad)
    replace alerts with message on rps-play.html
    
    rps-start.html
    put button link to rps-play in rps-js
    
    rps-play
    added button images
    added background image
    CSS changes
    
    need
    center scissor icon
    transparent background that covers icon slighly
    further CSS layout improvemennts
    Either winning message or link to a winning page
    edit icon images to black background,

--- rps-js.js
+++ rps-js.js
@@ -1,86 +1,136 @@
 
-      
 
+ 
+
+
+
+//<script src="rps-js.js"></script>
+
+const cScore = document.querySelector("#cScore");
+const pScore = document.querySelector("#pScore")
 const playerSelection = ["ROCK", "PAPER", "SCISSOR"];
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissor = document.querySelector("#scissor");
-const roundNum = document.querySelector("#roundNum")
+const round = document.querySelector("#round")
+let roundNum;
+let playerScore = 0
+
+
+
    
-function selectRock() {playRound(computerPlay(), "ROCK");}
-function selectScissor() {playRound(computerPlay(), "SCISSOR");}
-function selectPaper() { playRound(computerPlay(), "PAPER");}
-  
+
 //Computers choice
 computerSelection = computerPlay()
 function computerPlay(randomChoice)
     {const compChoices = ["ROCK", "PAPER", "SCISSOR", "PAPER", "ROCK", "SCISSOR","ROCK", "PAPER", "SCISSOR", "PAPER", "ROCK", "SCISSOR", "ROCK", "PAPER", "SCISSOR"];
     randomChoice = compChoices[Math.floor(Math.random() * 15)];
     return randomChoice;}
-    
+ 
+    function score(playerScore, computerScore) {  
+      if (point = 3)
+      { playerScore = 1 + playerScore}
+      else if (point = 2)
+      { computerScore = computerScore + 1}
+      else {return;}
+   }   
 //playRound
     function playRound(computerSelection, playerSelection) 
     {
    //Tie
-  
+
+
         if (playerSelection == "ROCK" && computerSelection == "ROCK" || playerSelection == "PAPER" && computerSelection == "PAPER" ||
       playerSelection == "SCISSOR" && computerSelection == "SCISSOR" )
-    { alert("Tie Game")}
+    { alert("Tie Game")
+  }
     // Player point
         else if (playerSelection == "ROCK" && computerSelection == "SCISSOR")
-        {   playerScore += 1;
-            alert("Player wins! Rock beats Scissor")}
+        {   
+          playerScore += 1;
+            alert("Player wins! Rock beats Scissor")
+          }
     
         else if (playerSelection == "SCISSOR" && computerSelection == "PAPER")
-        {playerScore += 1;
-            alert("Player wins! Scissor beats Paper")}
+        {   
+          playerScore = 1 + playerScore
+            alert("Player wins! Scissor beats Paper")
+            }
     
         else if (playerSelection == "PAPER" && computerSelection == "ROCK")
-        {playerScore += 1;
-            alert("Player wins! Paper covers Rock")}
+        { 
+          playerScore = 1 + playerScore
+            alert("Player wins! Paper covers Rock")
+            }
     
         // Computer point
         else if (playerSelection == "ROCK" && computerSelection == "PAPER")
-        {computerScore += 1;
-            alert("Player loses! Paper covers Rock")}
+        {
+            alert("Player loses! Paper covers Rock")
+            computerScore += 1;}
     
         else if (playerSelection == "SCISSOR" && computerSelection == "ROCK")
-        {computerScore += 1;
-            alert("Player loses! Rock beats Scissors")}
+        {
+            alert("Player loses! Rock beats Scissors")
+            computerScore += 1;}
     
         else if (playerSelection == "PAPER" && computerSelection == "SCISSOR")
-        {computerScore += 1;
-            alert("Player loses! Scissor cuts Paper")}
-    
-        else {alert("error - invalid input")};
-        }
+        {
+            alert("Player loses! Scissor cuts Paper")
+            computerScore += 1;};
+         
+            roundNum += 1;
+            if (roundNum === 6)
+            {results(playerScore, computerScore);
+              game()}
 
-    function game() { 
-   let playerScore = 0;
-    let computerScore = 0;
-    let roundNum = 1;
-   
-    for (i = 1 ; i < 6; i++) {
-     
-      
-      rock.addEventListener("click", selectRock);
-      scissor.addEventListener("click", selectScissor);
-      paper.addEventListener("click", selectPaper);
-     
-       
-        roundNum++;
-       
-      //    alert("Round 2")
-       // playRound();
-        //alert("Player Score: " + playerScore + " Computer Score: " + computerScore)
-        function results(playerScore, computerScore) {
+cScore.textContent = 'Computer Score: ' + computerScore;
+pScore.textContent = 'Player Score: ' + playerScore;
+round.textContent = "Round: " + roundNum; };
+    
+     //   else {alert("error - invalid input")};
+        
+
+function selectRock() {playRound(computerPlay(), "ROCK");};
+function selectScissor() {playRound(computerPlay(), "SCISSOR")};
+function selectPaper() { playRound(computerPlay(), "PAPER")}; 
+
+ function results(playerScore, computerScore) {
       if ( playerScore > computerScore){alert("Player wins the match!")}
       else if ( playerScore < computerScore) {alert("Player loses the match!")}
-      else { alert("Tie Game!");} }}
-  }
-          
+      else { alert("Tie Game!");} }
+
+
+
+function game() { 
+ 
+playerScore = 0;
+computerScore = 0;
+roundNum = 1;
+cScore.textContent = 'Computer Score: ' + computerScore;
+pScore.textContent = 'Player Score: ' + playerScore;
+round.textContent = "Round: " + roundNum; 
+
+rock.addEventListener("click", selectRock);
+scissor.addEventListener("click", selectScissor);
+paper.addEventListener("click", selectPaper);
+}
+            
     
-   console.log(playerSelection, computerSelection)
+    
+
+
+   const startBtn = document.querySelector("#startBtn");
+  startBtn.addEventListener("click", () => { window.location.href = "rps-play.html"});
+ 
+  
+       
+      
+   
+          
+
+       
+   
 
    
 
